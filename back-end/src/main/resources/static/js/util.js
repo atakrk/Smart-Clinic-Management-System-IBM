5cc23f34c42b9e7c1ae6a4852a472eb0bcf76e51
Ata Kürük <atakuruk@gmail.com>
2026-02-20 00:16:44 +0300

    frontend side

---
+++ back-end/src/main/resources/static/js/util.js
@@ -0,0 +1,12 @@
+// util.js
+  function setRole(role) {
+    localStorage.setItem("userRole", role);
+  }
+
+  function getRole() {
+    return localStorage.getItem("userRole");
+  }
+
+  function clearRole() {
+    localStorage.removeItem("userRole");
+  }
